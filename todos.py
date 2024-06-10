4939b9c15951a280992784967d17bc0e3a8c808f
Mike <129227664+mikexbal@users.noreply.github.com>
2024-06-10 18:39:13 -0400

    listify.py

--- todos.py
+++ todos.py
@@ -1,44 +1,67 @@
-import sys
+def showList():
+  if len(taskList) == 0:
+    print("\nYour list is currently empty!")
+  else: 
+    print(taskList)
 
-list = []
+def add():
+  task = input("\nEnter task: ")
+  taskList.append(task)
+  position = taskList.index(task)
+  taskList[position] = (f'{position + 1}. {task}')
+  
+def remove():
+  #Remove from list
+  print(taskList)
+  task = int(input("\nEnter task number: "))
 
-#Reads the file
-try:
-    file = open('to_do_list.txt', 'r')
-    list = file.readlines()
-    file.close()
-except:
-    pass
+  print(f"\n {taskList[task - 1]} removed!")
+  taskList.pop(task - 1)
 
-#Adds Item to List
-if len(sys.argv) >= 3 and sys.argv[1].lower() == 'add':
-    list.append(f"{(sys.argv[2])}\n")
+def complete():
+  taskNum = int(input("Enter task completed: ")) - 1
+  if taskNum < 0 or taskNum > len(taskList):
+    print("Invalid task number entered!")
+  else: 
+    task = taskList[taskNum]
+    print(f"You have completed {task}!")
+    taskList.pop(taskNum)
 
-#Removes Item from List
-if len(sys.argv) >= 3 and sys.argv[1].lower() == 'remove':
-    try:
-        item_deleted = int(sys.argv[2])
-        if item_deleted > 0:
-            item_deleted -= 1
-            del(list[item_deleted])
-        else:
-            print("Invalid number")
-            sys.exit(1)
-    except Exception as e:
-        print(e)
-        sys.exit(1)
+def deleteList():
+  taskList.clear()
+  print("\nList cleared!")
 
-#Views List
-if len(list) == 0:
-    print("Your To Do List is empty!")
-else:
-    print("\nHere's you To Do list:\n")
-    print("______________________\n")
-    for x in range(len(list)):
-        print(f"{x+1}. {list[x]}", end="")
+isRunning = True
+taskList = []
 
-
-#Saves the List
-file = open('to_do_list.txt', 'w')
-list = file.writelines(list)
-file.close()
+while isRunning:
+  print("\n**********************")
+  print("  Welcome to Listify  ")
+  print("**********************")
+  print("1. Show List")
+  print("2. Add task")
+  print("3. Remove task")
+  print("4. Mark task complete")
+  print("5. Clear list")
+  print("6. Exit")
+  print("**********************\n")
+  choice = input("Enter your choice (1-4): ")
+  
+  if choice == "1":
+    showList()
+  elif choice == "2":
+    add()
+  elif choice == "3":
+    remove()
+  elif choice == "4":
+    complete()
+  elif choice == "5":
+    deleteList()
+  elif choice == "6":
+    print("\nThanks for using Listify! Enjoy your day.")
+    isRunning = False
+  else:
+    print("Invalid choice! Choose and option from list. \n")
+    
+    
+  
